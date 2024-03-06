703cf3d71c904249a628e666a8e467be22ab3395
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-06 07:53:38 -0500

    added sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">SpecialOffer</section>
+      <section className="padding">CustomerReviews</section>
+      <section className="padding">Subscribe</section>
+      <section className="padding">Footer</section>
       <p class="bg-red-200 text-red-700 p-4 text-center font-bold">
         👨‍💻 Currently under construction! 🚧 <br />
         Just me tinkering with the code. Stay tuned for updates! 🚀
       </p>
-    </div>
+    </main>
   );
 };
 
