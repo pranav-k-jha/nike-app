168a03f880cc608ca1e2db24ad5b46eede12b8f8
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-06 06:44:06 -0500

    added under constuction message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <p class="bg-red-200 text-red-700 p-4 text-center font-bold">
+        👨‍💻 Currently under construction! 🚧 <br />
+        Just me tinkering with the code. Stay tuned for updates! 🚀
+      </p>
+    </div>
+  );
+};
 
-export default App
+export default App;
