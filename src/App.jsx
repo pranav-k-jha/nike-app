539ebfcc9f132ce9acec2aca6b361abd54b21d9d
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-06 14:22:29 -0500

    added links to nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import {
   Subscribe,
   Footer,
 } from "./sections";
+import Nav from "./components/Nav";
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav /> */}
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b"><Hero /></section>
       <section className="padding">
         <PopularProducts />
       </section>
