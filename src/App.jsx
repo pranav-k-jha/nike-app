520be492e3970e10d0742504ac9526124a430dd6
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-06 08:52:34 -0500

    completed adding file and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      {/* <Nav /> */}
       <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffer</section>
-      <section className="padding bg-pale-blue">CustomerReviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
       <p class="bg-red-200 text-red-700 p-4 text-center font-bold">
         👨‍💻 Currently under construction! 🚧 <br />
         Just me tinkering with the code. Stay tuned for updates! 🚀
